65382ae522a39eeab56d6413b3e8c87c3d243258
Michal Tichý <michal.tichy@riganti.cz>
2019-12-09 18:53:41 +0100

    added basic parametrization

--- IMS/Vole.h
+++ IMS/Vole.h
@@ -10,11 +10,11 @@ public:
 
 	Vole(unsigned int adultGen, int height, int width);
 	void ResolveCollision(std::unique_ptr<Cell>* colliderPtr, std::vector<std::vector<std::unique_ptr<Cell>>>* field) override;
+	unsigned int adultGeneration;
 protected:
 	std::unique_ptr<Cell>* FindClose(CellTypes desiredType, std::vector<std::vector<std::unique_ptr<Cell>>>* field);
 	std::unique_ptr<Cell>* FindFar(CellTypes desiredType, std::vector<std::vector<std::unique_ptr<Cell>>>* field);
 	std::unique_ptr<Cell>* Find(CellTypes desiredType, std::vector<std::vector<std::unique_ptr<Cell>>>* field, int distance);
 	void GoToDirection(int height, int width, int maxHeight, int maxWidth);
 	void GoToRandomDirection(int maxHeight, int maxWidth);
-	unsigned int adultGeneration;
 };
